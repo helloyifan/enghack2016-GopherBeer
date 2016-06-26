ffbb14a173913895034d25910cc53908609c81d2
Yifan <yfjd01@gmail.com>
2016-06-26 05:08:32 -0400

    working

--- app/src/main/java/com/example/yifandai/gopherbeer/main.java
+++ app/src/main/java/com/example/yifandai/gopherbeer/main.java
@@ -10,6 +10,7 @@ import android.hardware.SensorManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -21,7 +22,19 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+import java.net.URLConnection.*;
+import java.net.HttpURLConnection.*;
+import java.net.URL.*;
+import java.net.*;
+import java.io.*;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 import org.json.JSONObject;
 import org.json.JSONTokener;
@@ -32,6 +45,14 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+
+
+import java.io.IOException;
+import java.util.Scanner;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class main extends AppCompatActivity implements LocationListener, SensorEventListener {
     protected LocationManager locationManager;
@@ -51,6 +72,11 @@ public class main extends AppCompatActivity implements LocationListener, SensorE
     double degree;
 
     ImageView imageView;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,11 +84,11 @@ public class main extends AppCompatActivity implements LocationListener, SensorE
 
 
         setContentView(R.layout.mainscreen);
-        SensorManager mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        SensorManager mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
         Sensor accelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         Sensor magnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
-        mSensorManager.registerListener(this, magnetometer,SensorManager.SENSOR_DELAY_NORMAL);
-        mSensorManager.registerListener(this, accelerometer,SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_NORMAL);
+        mSensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         location1 = new Location("location 1");
         userLocation = new Location("user location");
@@ -74,47 +100,122 @@ public class main extends AppCompatActivity implements LocationListener, SensorE
 
         location1.setLatitude(location1lat);
         location1.setLongitude(location1lng);
-        imageView = (ImageView)findViewById(R.id.arrow);
+        imageView = (ImageView) findViewById(R.id.arrow);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client2 = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
 
-    public void getLatLng(){
+    public void getLatLng() {
         String locationProvider = LocationManager.NETWORK_PROVIDER;
         Location lastKnownLocation = locationManager.getLastKnownLocation(locationProvider);
         lat = lastKnownLocation.getLatitude();
         lng = lastKnownLocation.getLongitude();
 
-        TextView tv = (TextView)findViewById(R.id.text);
-        tv.setText(lat+","+lng);
-        userLocation.setLatitude(lat);
-        userLocation.setLongitude(lng);
+        TextView tv = (TextView) findViewById(R.id.text);
+        //tv.setText(lat + "," + lng);
+        //userLocation.setLatitude(lat);
+        //userLocation.setLongitude(lng);
     }
 
-    public void function(View v){
+    public void function(View v) {
         getLatLng();
     }
 
-    public void beerClicked(View v){
+    public void beerClicked(View v) {
         distance = userLocation.distanceTo(location1);
-        TextView distancetv = (TextView)findViewById(R.id.distance);
-        distancetv.setText(""+distance);
+        TextView distancetv = (TextView) findViewById(R.id.distance);
+        distancetv.setText("" + distance);
         degree = userLocation.bearingTo(location1);
-
-
-
     }
 
-    public void apiCall(View v){
-        ImageView iv = (ImageView)findViewById(R.id.arrow);
-        iv.setRotation(180);
+
+    OkHttpClient client = new OkHttpClient();
+
+    String run(String url) throws IOException {
+        Log.d("run ran", url);
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        Log.d("flag", "flag");
+        Response response = client.newCall(request).execute();
+
+        Log.d("please", "please");
+        return response.body().string();
     }
 
+    static String convertStreamToString(InputStream is) {
+        Scanner s = new Scanner(is).useDelimiter("\\A");
+        return s.hasNext() ? s.next() : "";
+    }
+    // Reads an InputStream and converts it to a String.
+    public String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
+        Reader reader = null;
+        reader = new InputStreamReader(stream, "UTF-8");
+        char[] buffer = new char[len];
+        reader.read(buffer);
+        return new String(buffer);
+    }
+
+    private String downloadUrl(String myurl) throws IOException {
+        InputStream is = null;
+        // Only display the first 500 characters of the retrieved
+        // web page content.
+        int len = 500;
+
+        try {
+            URL url = new URL(myurl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            // Starts the query
+            conn.connect();
+            int response = conn.getResponseCode();
+            Log.d("","The response is: " + response);
+            is = conn.getInputStream();
+
+            // Convert the InputStream into a string
+            String contentAsString = readIt(is, len);
+            return contentAsString;
+
+            // Makes sure that the InputStream is closed after the app is
+            // finished using it.
+        } finally {
+            if (is != null) {
+                is.close();
+            }
+        }
+    }
+
+
+
+    public void apiCallOne(View v) {
+        //"http://ontariobeerapi.ca:80/stores/?city=Waterloo"
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            try {
+                String retVal = downloadUrl("http://ontariobeerapi.ca:80/stores/?city=Waterloo");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        } else {
+            Log.d("Error", "Error");
+        }
+
+        Log.d("Flag1b", "Flag1b");
+        Log.d("Flag2", "Flag2");
+        Log.d("Flag3", "Flag3");
+    }
 
 
     @Override
     public void onLocationChanged(Location location) {
-        Toast.makeText(getBaseContext(), "eeee", Toast.LENGTH_LONG).show();
-
+        Toast.makeText(getBaseContext(), "Location Changed", Toast.LENGTH_LONG).show();
         Log.d("hello", "hello");
     }
 
@@ -132,8 +233,10 @@ public class main extends AppCompatActivity implements LocationListener, SensorE
     public void onProviderDisabled(String s) {
 
     }
+
     float[] mGravity;
     float[] mGeomagnetic;
+
     @Override
     public void onSensorChanged(SensorEvent event) {
 
@@ -150,25 +253,25 @@ public class main extends AppCompatActivity implements LocationListener, SensorE
                 float orientation[] = new float[3];
                 SensorManager.getOrientation(R, orientation);
                 float azimut = orientation[0];
-                int degrees = (int)Math.round(Math.toDegrees(azimut));
+                int degrees = (int) Math.round(Math.toDegrees(azimut));
 
-                int xConstant = (int)degree;
-                if(xConstant < 0){
+                int xConstant = (int) degree;
+                if (xConstant < 0) {
                     xConstant = xConstant + 360;
                 }
                 int yVariable = degrees;
-                if(yVariable < 0){
+                if (yVariable < 0) {
                     yVariable = yVariable + 360;
                 }
 
                 int zDifference = xConstant - yVariable;
-                if(zDifference < 0){
+                if (zDifference < 0) {
                     zDifference = zDifference + 360;
                 }
 
-                if (imageView != null){
-                    imageView.setRotation((float)(zDifference));
-                    Log.d(""+xConstant,""+yVariable+ "   "+zDifference);
+                if (imageView != null) {
+                    imageView.setRotation((float) (zDifference));
+                    //(""+xConstant,""+yVariable+ "   "+zDifference);
 
                 }
             }
@@ -178,5 +281,45 @@ public class main extends AppCompatActivity implements LocationListener, SensorE
     @Override
     public void onAccuracyChanged(Sensor sensor, int i) {
 
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client2.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.yifandai.gopherbeer/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client2, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.example.yifandai.gopherbeer/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client2, viewAction);
+        client2.disconnect();
     }
 }
